a87bfc570389bee7bd04199521409d10e7418803
yuanw <me@yuanwang.ca>
2026-02-23 21:53:17 -0600

    add

--- flake.nix
+++ flake.nix
@@ -4,44 +4,44 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    
-    git-hooks.url = "github:cachix/git-hooks.nix";
-    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+
+
     emacs = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-    };    
+    };
   };
 
-  outputs = inputs@{flake-parts, ...}:
-  flake-parts.lib.mkFlake {inherit inputs;} {
-    systems = [
-      "x86_64-linux"
-    ];
-    imports = [
-     inputs.git-hooks.flakeModule     
-    ];
-    perSystem = { config, pkgs, system, ... }: {
-  _module.args.pkgs = import inputs.nixpkgs {
-    inherit system;
-    overlays = [
-      inputs.emacs.overlay
-      (final: prev: {
-        # ... things you need to patch ...
-      })
-    ];
-    config = { };
-  };
-  pre-commit.settings.hooks = {
-        nixpkgs-fmt.enable = true;    
-  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
+      perSystem = { config, pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.emacs.overlay
+            (final: prev: {
+              # ... things you need to patch ...
+            })
+          ];
+          config = { };
+        };
+        pre-commit.settings.hooks = {
+          nixpkgs-fmt.enable = true;
+        };
 
-  devShells.default = pkgs.mkShell {
-    shellHook = ''
-      ${config.pre-commit.shellHook}
-      '';
-  };
-};
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ${config.pre-commit.shellHook}
+          '';
+        };
+      };
 
-  };
+    };
 }
