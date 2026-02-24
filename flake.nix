5dc5fcdbdfa3b55ee9c53b28362f2e9272726041
yuanw <me@yuanwang.ca>
2026-02-23 21:30:21 -0600

    add basic flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Generate emacs manual in epub format";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.flake = false;
+    emacs = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };    
+  };
+
+  outputs = inputs@{flake-parts, ...}:
+  flake-parts.lib.mkFlake {inherit inputs;} {
+    systems = [
+      "x86_64-linux"
+    ];
+    imports = [
+    (inputs.git-hooks + /flake-module.nix)      
+    ];
+    perSystem = { pkgs, system, ... }: {
+  _module.args.pkgs = import inputs.nixpkgs {
+    inherit system;
+    overlays = [
+      inputs.emacs.overlay
+      (final: prev: {
+        # ... things you need to patch ...
+      })
+    ];
+    config = { };
+  };
+  pre-commit.settings = {
+        nixpkgs-fmt.enable = true;    
+  };
+
+  devShells.default = pkgs.mkShell {
+  };
+};
+
+  };
+}
